90c91bb75284020385e87e805a9bc0981659124d
Gabrielle Wee <mail@gabriellew.ee>
2021-03-13 13:44:46 -0800

    add page filters

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
-var lazyLoadInstance = new LazyLoad();
+let lazyLoadInstance = new LazyLoad();
+
 let iso = new Isotope(document.querySelector('.grid-isotope'), {
 	percentPosition: true,
 	layoutMode: 'packery',
@@ -12,3 +13,25 @@ new imagesLoaded(window, { background: true }, function(){
 		}, 500);
 	}, 100);
 });
+
+let filterList = document.querySelector(".grid-filters");
+if (filterList != null) {
+	let filterLinks = filterList.querySelectorAll(".grid-filter");
+	filterLinks.forEach(filterLink=>{
+		let filterValue = filterLink.getAttribute("data-filter");
+		filterLink.addEventListener("click", function(event) {
+			event.preventDefault();
+			iso.arrange({ filter: filterValue });
+			let filterActive = filterList.querySelector(".active");
+			if (filterActive != null && filterValue == "*") {
+				filterActive.classList.remove("active");
+			} else if (filterActive != null) {
+				filterActive.classList.remove("active");
+				event.target.classList.add("active");
+			} else {
+				event.target.classList.add("active");
+			};
+			iso.layout();
+		});
+	});
+}
