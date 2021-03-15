220283ae4fe23123280a3d3cf1bbca9490f048e1
Gabrielle Wee <mail@gabriellew.ee>
2021-03-15 13:25:18 -0700

    add ax and focus properties

--- static/js/main.js
+++ static/js/main.js
@@ -14,24 +14,56 @@ new imagesLoaded(window, { background: true }, function(){
 	}, 100);
 });
 
-let filterList = document.querySelector(".grid-filters");
+let filterList = document.querySelector('.grid-filters');
 if (filterList != null) {
-	let filterLinks = filterList.querySelectorAll(".grid-filter");
-	filterLinks.forEach(filterLink=>{
-		let filterValue = filterLink.getAttribute("data-filter");
-		filterLink.addEventListener("click", function(event) {
+	let filterLinks = filterList.querySelectorAll('[data-filter]');
+	filterLinks.forEach(el=>{
+		let value = el.getAttribute('data-filter');
+		el.addEventListener('click', function(event) {
 			event.preventDefault();
-			iso.arrange({ filter: filterValue });
-			let filterActive = filterList.querySelector(".active");
-			if (filterActive != null && filterValue == "*") {
-				filterActive.classList.remove("active");
-			} else if (filterActive != null) {
-				filterActive.classList.remove("active");
-				event.target.classList.add("active");
+			iso.arrange({ filter: value });
+			let active = filterList.querySelector('.active');
+			if (active != null && value == '*') {
+				active.classList.remove('active');
+			} else if (active != null) {
+				active.classList.remove('active');
+				event.target.classList.add('active');
 			} else {
-				event.target.classList.add("active");
+				event.target.classList.add('active');
 			};
 			iso.layout();
 		});
 	});
 }
+
+let infoInputs = Array.from(document.querySelectorAll('.grid-input'));
+infoInputs.forEach(el=>{
+	let info = el.nextElementSibling.nextElementSibling.nextElementSibling;
+	let external = info.querySelector('.grid-external');
+	info.setAttribute('aria-hidden', 'false');
+	el.addEventListener('keypress', e => {
+		if (e.keyCode == 13) {
+			if(el.checked){
+				el.checked = false;
+				external.tabIndex = -1;
+				info.setAttribute('aria-hidden', 'true');
+			} else {
+				el.checked = true;
+				external.tabIndex = 0;
+				info.setAttribute('aria-hidden', 'false');
+			}
+		}
+	});
+});
+
+let tagsInput = document.querySelector('#grid-filters');
+tagsInput.addEventListener('keypress',  e => {
+	if (e.keyCode == 13) {
+		if(tagsInput.checked){
+			tagsInput.checked = false;
+		} else {
+			tagsInput.checked = true;
+		}
+	}
+});
+
