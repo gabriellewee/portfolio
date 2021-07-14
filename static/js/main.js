ee72781baa96788bf213b788b2222ea2df10b6ff
Gabrielle Wee <mail@gabriellew.ee>
2021-07-14 16:48:11 -0700

    add wraparound and background adjustments for lightbox

--- static/js/main.js
+++ static/js/main.js
@@ -73,11 +73,10 @@ if(tagsInput != null) {
 
 let expandLinks = document.querySelectorAll('.expand');
 let lightboxLinks = document.querySelectorAll('.lightbox');
-console.log(expandLinks);
-console.log(lightboxLinks);
+let lightboxSibling;
 if(expandLinks != null && lightboxLinks != null) {
 	expandLinks.forEach((el, index)=>{
-		let spanLink = lightboxLinks[index].querySelector('span');
+		let spanLink = lightboxLinks[index].querySelector("span");
 		el.addEventListener('click', e => {
 			e.preventDefault();
 			lightboxLinks[index].classList.add("active");
@@ -86,7 +85,7 @@ if(expandLinks != null && lightboxLinks != null) {
 			});
 		});
 	});
-	lightboxLinks.forEach(el=>{
+	lightboxLinks.forEach((el, index)=>{
 		el.addEventListener('click', e => {
 			e.preventDefault();
 			el.classList.remove("active");
@@ -94,9 +93,23 @@ if(expandLinks != null && lightboxLinks != null) {
 		});
 		document.addEventListener('keydown', e => {
 			let keyCode = e.keyCode;
-			if (keyCode === 27) {
-				el.classList.remove("active");
-				el.querySelector('span').classList.remove("active");
+			if(el.classList.contains("active")) {
+				if (keyCode === 27) {
+					el.classList.remove("active");
+					el.querySelector("span").classList.remove("active");
+				} else if(keyCode === 39 || keyCode === 37) {
+					setTimeout(() => {
+						el.classList.remove("active");
+						el.querySelector("span").classList.remove("active");
+						if(keyCode === 39) {
+							lightboxSibling = lightboxLinks[index + 1] || lightboxLinks[0];
+						} else if(keyCode === 37) {
+							lightboxSibling = lightboxLinks[index - 1] || lightboxLinks[lightboxLinks.length - 1];
+						}
+						lightboxSibling.classList.add("active");
+						lightboxSibling.querySelector("span").classList.add("active");
+					}, 100);
+				}
 			}
 		});
 	});
