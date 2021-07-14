2410335f12c2d31257a75f47ebf53f3dd4cea456
Gabrielle Wee <mail@gabriellew.ee>
2021-07-14 15:58:46 -0700

    add lightbox to index

--- static/js/main.js
+++ static/js/main.js
@@ -37,59 +37,67 @@ if (filterList != null) {
 }
 
 let infoInputs = Array.from(document.querySelectorAll('.grid-input'));
-infoInputs.forEach(el=>{
-	let info = el.nextElementSibling.nextElementSibling.nextElementSibling;
-	let expand = info.querySelector('.grid-expand');
-	info.setAttribute('aria-hidden', 'false');
-	el.addEventListener('keypress', e => {
+if(infoInputs != null) {
+	infoInputs.forEach(el=>{
+		let info = el.nextElementSibling.nextElementSibling.nextElementSibling;
+		let expand = info.querySelector('.grid-expand');
+		info.setAttribute('aria-hidden', 'false');
+		el.addEventListener('keypress', e => {
+			if (e.keyCode == 13) {
+				if(el.checked){
+					el.checked = false;
+					expand.tabIndex = -1;
+					info.setAttribute('aria-hidden', 'true');
+				} else {
+					el.checked = true;
+					expand.tabIndex = 0;
+					info.setAttribute('aria-hidden', 'false');
+				}
+			}
+		});
+	});
+}
+
+let tagsInput = document.querySelector('#grid-filters');
+if(tagsInput != null) {
+	tagsInput.addEventListener('keypress',  e => {
 		if (e.keyCode == 13) {
-			if(el.checked){
-				el.checked = false;
-				expand.tabIndex = -1;
-				info.setAttribute('aria-hidden', 'true');
+			if(tagsInput.checked){
+				tagsInput.checked = false;
 			} else {
-				el.checked = true;
-				expand.tabIndex = 0;
-				info.setAttribute('aria-hidden', 'false');
+				tagsInput.checked = true;
 			}
 		}
 	});
-});
+}
 
-let tagsInput = document.querySelector('#grid-filters');
-tagsInput.addEventListener('keypress',  e => {
-	if (e.keyCode == 13) {
-		if(tagsInput.checked){
-			tagsInput.checked = false;
-		} else {
-			tagsInput.checked = true;
-		}
-	}
-});
-
-let expandLinks = document.querySelectorAll('.grid-expand');
+let expandLinks = document.querySelectorAll('.expand');
 let lightboxLinks = document.querySelectorAll('.lightbox');
-expandLinks.forEach((el, index)=>{
-	let spanLink = lightboxLinks[index].querySelector('span');
-	el.addEventListener('click', e => {
-		e.preventDefault();
-		lightboxLinks[index].classList.add("active");
-		imagesLoaded(spanLink, { background: true }, function(){
-			spanLink.classList.add("active");
+console.log(expandLinks);
+console.log(lightboxLinks);
+if(expandLinks != null && lightboxLinks != null) {
+	expandLinks.forEach((el, index)=>{
+		let spanLink = lightboxLinks[index].querySelector('span');
+		el.addEventListener('click', e => {
+			e.preventDefault();
+			lightboxLinks[index].classList.add("active");
+			imagesLoaded(spanLink, { background: true }, function(){
+				spanLink.classList.add("active");
+			});
 		});
 	});
-});
-lightboxLinks.forEach(el=>{
-	el.addEventListener('click', e => {
-		e.preventDefault();
-		el.classList.remove("active");
-		el.querySelector('span').classList.remove("active");
-	});
-	document.addEventListener('keydown', e => {
-		let keyCode = e.keyCode;
-		if (keyCode === 27) {
+	lightboxLinks.forEach(el=>{
+		el.addEventListener('click', e => {
+			e.preventDefault();
 			el.classList.remove("active");
 			el.querySelector('span').classList.remove("active");
-		}
+		});
+		document.addEventListener('keydown', e => {
+			let keyCode = e.keyCode;
+			if (keyCode === 27) {
+				el.classList.remove("active");
+				el.querySelector('span').classList.remove("active");
+			}
+		});
 	});
-});
+}
