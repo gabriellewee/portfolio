851cc968241d967023f1337f05db917c86c8fd30
Gabrielle Wee <mail@gabriellew.ee>
2022-06-27 17:39:20 -0700

    clean up js and add some accessible styles

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,68 +1,67 @@
 const lightbox = ((links = Array.from(document.querySelectorAll(".expand"))) => {
 	let lightboxes = Array.from(document.querySelectorAll(".lightbox"));
 	let sibling;
-	if(links && lightboxes) {
-		links.forEach((link, index)=>{
-			let backgrounds = Array.from(lightboxes[index].querySelectorAll(".image"));
-			link.addEventListener("click", e => {
-				e.preventDefault();
-				lightboxes[index].classList.add("active");
-				lightboxes[index].focus();
-				backgrounds.forEach(background=>{
-					imagesLoaded(background, () => {
-						background.classList.add("active");
-					});
+	if(!links && !lightboxes) return;
+	links.forEach((link, index)=>{
+		let backgrounds = Array.from(lightboxes[index].querySelectorAll(".image"));
+		link.addEventListener("click", e => {
+			e.preventDefault();
+			lightboxes[index].classList.add("active");
+			lightboxes[index].focus();
+			backgrounds.forEach(background=>{
+				imagesLoaded(background, () => {
+					background.classList.add("active");
 				});
 			});
 		});
-		lightboxes.forEach((lightbox, index)=>{
-			let backgrounds = Array.from(lightbox.querySelectorAll(".image"));
-			lightbox.addEventListener("click", e => {
-				e.preventDefault();
-				lightbox.classList.remove("active");
-				links[index].focus();
-				backgrounds.forEach(background=>{
-					background.classList.remove("active");
-				});
+	});
+	lightboxes.forEach((lightbox, index)=>{
+		let backgrounds = Array.from(lightbox.querySelectorAll(".image"));
+		lightbox.addEventListener("click", e => {
+			e.preventDefault();
+			lightbox.classList.remove("active");
+			links[index].focus();
+			backgrounds.forEach(background=>{
+				background.classList.remove("active");
 			});
-			document.addEventListener("scroll", e => {
-				if(lightbox.classList.contains("active")) {
+		});
+		document.addEventListener("scroll", e => {
+			if(lightbox.classList.contains("active")) {
+				setTimeout(() => {
+					lightbox.classList.remove("active");
+					backgrounds.forEach(background=>{
+						background.classList.remove("active");
+					});
+				}, 300);
+			}
+		});
+		document.addEventListener("keydown", e => {
+			if(lightbox.classList.contains("active")) {
+				if (e.key === "Escape") {
+					lightbox.classList.remove("active");
+					links[index].focus();
+					backgrounds.forEach(background=>{
+						background.classList.remove("active");
+					});
+				} else if(e.key === "ArrowRight" || e.key === "ArrowLeft") {
 					setTimeout(() => {
 						lightbox.classList.remove("active");
 						backgrounds.forEach(background=>{
 							background.classList.remove("active");
 						});
-					}, 300);
-				}
-			});
-			document.addEventListener("keydown", e => {
-				if(lightbox.classList.contains("active")) {
-					if (e.key === "Escape") {
-						lightbox.classList.remove("active");
-						links[index].focus();
-						backgrounds.forEach(background=>{
-							background.classList.remove("active");
+						if(e.key === "ArrowRight") {
+							sibling = lightboxes[index + 1] || lightboxes[0];
+						} else if(e.key === "ArrowLeft") {
+							sibling = lightboxes[index - 1] || lightboxes[lightboxes.length - 1];
+						}
+						let siblingBackgrounds = Array.from(sibling.querySelectorAll(".image"));
+						sibling.classList.add("active");
+						siblingBackgrounds.forEach(background=>{
+							background.classList.add("active");
 						});
-					} else if(e.key === "ArrowRight" || e.key === "ArrowLeft") {
-						setTimeout(() => {
-							lightbox.classList.remove("active");
-							backgrounds.forEach(background=>{
-								background.classList.remove("active");
-							});
-							if(e.key === "ArrowRight") {
-								sibling = lightboxes[index + 1] || lightboxes[0];
-							} else if(e.key === "ArrowLeft") {
-								sibling = lightboxes[index - 1] || lightboxes[lightboxes.length - 1];
-							}
-							let siblingBackgrounds = Array.from(sibling.querySelectorAll(".image"));
-							sibling.classList.add("active");
-							siblingBackgrounds.forEach(background=>{
-								background.classList.add("active");
-							});
-						}, 100);
-					}
+					}, 100);
 				}
-			});
+			}
 		});
-	}
+	});
 })();
--- static/js/main.js
+++ static/js/main.js
@@ -1,67 +1,6 @@
-const noJS = (() => {
-	document.documentElement.classList.remove("no-js");
-	document.documentElement.classList.add("js");
-})();
-
-const clipboardCopy = (() => {
-	let container = document.querySelector(".discord");
-	if(container) {
-		let discordWindow = container.querySelector(".discord-window");
-		let discordArrow = container.querySelector(".discord-label");
-
-		let copyTrigger = container.querySelector("input");
-		let closeTrigger = container.querySelector(".close");
-		let triggers = Array.from([copyTrigger, closeTrigger]);
-		
-		let copyElem = container.querySelector(".username");
-		
-		triggers.forEach(trigger =>{
-			trigger.addEventListener("keydown",  e => {
-				if (e.key === "Enter") {
-					if(copyTrigger.checked) {
-						copyTrigger.checked = false;
-						copyElem.classList.remove("copied");
-						copyTrigger.focus();
-					} else {
-						copyTrigger.checked = true;
-					}
-				}
-			});
-		});
-
-		let copyButton = container.querySelector(".copy-button");
-		let clipboard = new ClipboardJS(copyButton);
-		clipboard.on("success", e => {
-			let copy = container.querySelector(e.trigger.getAttribute("data-clipboard-target"));
-			copy.parentNode.classList.add("copied");
-		    e.clearSelection();
-		});
-		copyButton.addEventListener("keydown", e => {
-			if (e.key === "Enter") {
-				setTimeout(() => {
-					closeTrigger.focus();
-				}, 50);
-			}
-		})
-
-		container.addEventListener("keydown",  e => {
-			if (e.key === "Escape") {
-				copyTrigger.checked = false;
-				copyElem.classList.remove("copied");
-				copyTrigger.focus();
-			}
-		});
-		document.addEventListener("scroll", e => {
-			if(copyTrigger.checked) {
-				setTimeout(() => {
-					copyTrigger.checked = false;
-					discordWindow.classList.remove("active");
-					discordArrow.classList.remove("active");
-					copyElem.classList.remove("copied");
-				}, 300);
-			}
-		});
-	}
+const noJS = ((container = document.documentElement) => {
+	container.classList.remove("no-js");
+	container.classList.add("js");
 })();
 
 let container = document.querySelector(".grid-isotope");
@@ -82,100 +21,157 @@ const loading = new imagesLoaded(document.body, { background: true }, () => {
 	}, 100);
 });
 
-const visualFilters = ((filters = document.querySelector(".grid-filters")) => {
-	if (filters) {
-		let tagTrigger = filters.querySelector("#grid-filters");
-		tagTrigger.addEventListener("keydown",  e => {
+const clipboardCopy = ((container = document.querySelector(".discord")) => {
+	if(!container) return;
+	let discordWindow = container.querySelector(".discord-window");
+	let discordArrow = container.querySelector(".discord-label");
+
+	let copyTrigger = container.querySelector("input");
+	let closeTrigger = container.querySelector(".close");
+	let triggers = Array.from([copyTrigger, closeTrigger]);
+	
+	let copyElem = container.querySelector(".username");
+	
+	triggers.forEach(trigger =>{
+		trigger.addEventListener("keydown",  e => {
 			if (e.key === "Enter") {
-				if (tagTrigger.checked) {
-					tagTrigger.checked = false;
-					tagTrigger.setAttribute("aria-checked", "false");
+				if(copyTrigger.checked) {
+					copyTrigger.checked = false;
+					copyElem.classList.remove("copied");
+					copyTrigger.focus();
 				} else {
-					tagTrigger.checked = true;
-					tagTrigger.setAttribute("aria-checked", "true");
+					copyTrigger.checked = true;
 				}
 			}
 		});
-		tagTrigger.addEventListener("click", e => {
-			if(tagTrigger.checked){
-				tagTrigger.setAttribute("aria-checked", "true");
-			} else {
+	});
+
+	let copyButton = container.querySelector(".copy-button");
+	let clipboard = new ClipboardJS(copyButton);
+	clipboard.on("success", e => {
+		let copy = container.querySelector(e.trigger.getAttribute("data-clipboard-target"));
+		copy.parentNode.classList.add("copied");
+	    e.clearSelection();
+	});
+	copyButton.addEventListener("keydown", e => {
+		if (e.key === "Enter") {
+			setTimeout(() => {
+				closeTrigger.focus();
+			}, 50);
+		}
+	})
+
+	container.addEventListener("keydown",  e => {
+		if (e.key === "Escape") {
+			copyTrigger.checked = false;
+			copyElem.classList.remove("copied");
+			copyTrigger.focus();
+		}
+	});
+	document.addEventListener("scroll", e => {
+		if(copyTrigger.checked) {
+			setTimeout(() => {
+				copyTrigger.checked = false;
+				discordWindow.classList.remove("active");
+				discordArrow.classList.remove("active");
+				copyElem.classList.remove("copied");
+			}, 300);
+		}
+	});
+})();
+
+const visualFilters = ((filters = document.querySelector(".grid-filters")) => {
+	if (!filters) return;
+	let tagTrigger = filters.querySelector("#grid-filters");
+	tagTrigger.addEventListener("keydown",  e => {
+		if (e.key === "Enter") {
+			if (tagTrigger.checked) {
+				tagTrigger.checked = false;
 				tagTrigger.setAttribute("aria-checked", "false");
+			} else {
+				tagTrigger.checked = true;
+				tagTrigger.setAttribute("aria-checked", "true");
 			}
+		}
+	});
+	tagTrigger.addEventListener("click", e => {
+		if(tagTrigger.checked){
+			tagTrigger.setAttribute("aria-checked", "true");
+		} else {
+			tagTrigger.setAttribute("aria-checked", "false");
+		}
+	});
+	let links = filters.querySelectorAll("[data-filter]");
+	links.forEach(link=>{
+		let value = link.getAttribute("data-filter");
+		link.addEventListener("click", e => {
+			e.preventDefault();
+			iso.arrange({ filter: value });
+			let active = filters.querySelector(".active");
+			if (active && value == "*") {
+				active.classList.remove("active");
+			} else if (active) {
+				active.classList.remove("active");
+				e.target.classList.add("active");
+			} else {
+				e.target.classList.add("active");
+			};
+			iso.layout();
 		});
-		let links = filters.querySelectorAll("[data-filter]");
-		links.forEach(link=>{
-			let value = link.getAttribute("data-filter");
-			link.addEventListener("click", function(e) {
-				e.preventDefault();
-				iso.arrange({ filter: value });
-				let active = filters.querySelector(".active");
-				if (active && value == "*") {
-					active.classList.remove("active");
-				} else if (active) {
-					active.classList.remove("active");
-					e.target.classList.add("active");
-				} else {
-					e.target.classList.add("active");
-				};
-				iso.layout();
-			});
-		});
-	}
+	});
 })();
 
 const visualInfoTriggers = ((figures = Array.from(document.querySelectorAll(".grid-figure"))) => {
-	if(figures) {
-		figures.forEach(figure=>{
-			let button = figure.querySelector(".grid-input")
-			let info = figure.querySelector(".grid-info");
-			let expand = figure.querySelector(".grid-expand");
-			let external = figure.querySelector(".external");
-			let name = button.getAttribute("id").slice(0, -5);
-			let labels = Array.from(figure.querySelectorAll("label"));
+	if(!figures) return;
+	figures.forEach(figure=>{
+		let button = figure.querySelector(".grid-input")
+		let info = figure.querySelector(".grid-info");
+		let expand = figure.querySelector(".grid-expand");
+		let external = figure.querySelector(".external");
+		let name = button.getAttribute("id").slice(0, -5);
+		let labels = Array.from(figure.querySelectorAll("label"));
+		info.setAttribute("aria-hidden", "true");
+		button.addEventListener("focus", e => {
+			document.getElementById(name).scrollIntoView({ behavior: "smooth" });
+		});
+		let _true = (() => {
+			button.setAttribute("aria-checked", "true");
+			info.removeAttribute("aria-hidden");
+			expand.tabIndex = 0;
+			if(external) external.tabIndex = 0;
+		});
+		let _false = (() => {
+			button.setAttribute("aria-checked", "false");
 			info.setAttribute("aria-hidden", "true");
-			button.addEventListener("focus", e => {
-				document.getElementById(name).scrollIntoView({ behavior: "smooth" });
-			});
-			let _true = (() => {
-				button.setAttribute("aria-checked", "true");
-				info.removeAttribute("aria-hidden");
-				expand.tabIndex = 0;
-				if(external) external.tabIndex = 0;
-			});
-			let _false = (() => {
-				button.setAttribute("aria-checked", "false");
-				info.setAttribute("aria-hidden", "true");
-				expand.tabIndex = -1;
-				if(external) external.tabIndex = -1;
-			});
-			button.addEventListener("click", e => {
+			expand.tabIndex = -1;
+			if(external) external.tabIndex = -1;
+		});
+		button.addEventListener("click", e => {
+			if(button.checked){
+				_true();
+			} else {
+				_false();
+			}
+		});
+		button.addEventListener("keydown", e => {
+			if (e.key === "Enter") {
+				if(button.checked){
+					button.checked = false;
+					_false();
+				} else {
+					button.checked = true;
+					_true();
+				}
+			}
+		});
+		labels.forEach(label=>{
+			label.addEventListener("click", e => {
 				if(button.checked){
 					_true();
 				} else {
 					_false();
 				}
 			});
-			button.addEventListener("keydown", e => {
-				if (e.key === "Enter") {
-					if(button.checked){
-						button.checked = false;
-						_false();
-					} else {
-						button.checked = true;
-						_true();
-					}
-				}
-			});
-			labels.forEach(label=>{
-				label.addEventListener("click", e => {
-					if(button.checked){
-						_true();
-					} else {
-						_false();
-					}
-				});
-			});
 		});
-	}
+	});
 })();
