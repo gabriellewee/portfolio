29c75c9c1ef0fa0da4d776c72743fc2474b64cfe
gabriellewee <mail@gabriellew.ee>
2023-10-10 11:43:45 -0700

    fix header layout/animation on mobile viewports

--- static/js/pages/home.js
+++ static/js/pages/home.js
@@ -4,6 +4,15 @@ let text = document.querySelector(".text");
 let posts = Array.from(document.querySelectorAll(".post-animate"));
 posts.push(document.querySelector(".bottom"));
 
+enter.fromTo(".message", {
+	opacity: 0,
+	y: "100%"
+}, {
+	duration: .1,
+	opacity: 1,
+	y: 0,
+	ease: "power1.out"
+});
 enter.set('.title', {
 	opacity: 1
 });
@@ -30,7 +39,8 @@ enter.set('.cursor', { className: 'cursor animated' });
 enter.to('.message', {
 	duration: .2,
 	opacity: 0,
-	y: "-100%"
+	y: "-100%",
+	ease: "power1.out"
 });
 enter.fromTo(".contact-sites", {
 	opacity: 0,
