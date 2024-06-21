a3c55e567abba697a98383c294cf4545655a87da
Gabrielle Wee <mail@gabriellew.ee>
2024-06-20 20:41:56 -0700

    fix infinite scroll unload lighthouse issue

--- _config/passthrough.js
+++ _config/passthrough.js
@@ -18,7 +18,6 @@ module.exports = eleventyConfig => {
 		"node_modules/gsap/dist/ScrollTrigger.min.js": "static/js/scroll-trigger.min.js",
 		"node_modules/gsap/dist/TextPlugin.min.js": "static/js/text.min.js",
 		"node_modules/imagesloaded/imagesloaded.pkgd.min.js": "static/js/imagesloaded.min.js",
-		"node_modules/infinite-scroll/dist/infinite-scroll.pkgd.min.js": "static/js/infinite-scroll.min.js",
 		"node_modules/isotope-layout/dist/isotope.pkgd.min.js": "static/js/isotope.min.js",
 		"node_modules/isotope-packery/packery-mode.pkgd.min.js": "static/js/packery.min.js",
 		"node_modules/luxon/build/global/luxon.min.js": "static/js/luxon.min.js",
