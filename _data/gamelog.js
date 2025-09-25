56bf152a6e7a94a77a5ef8142d84917309b9f647
Gabrielle Wee <mail@gabriellew.ee>
2025-09-25 12:36:46 -0700

    update notion/threads api

--- _data/gamelog.js
+++ _data/gamelog.js
@@ -2,7 +2,7 @@ import Cache from '@11ty/eleventy-fetch';
 
 export default async function() {
 	try {
-		const response = await Cache(`https://api.notion.com/v1/databases/${process.env.NOTION_GAMELOG}/query`, {
+		const response = await Cache(`https://api.notion.com/v1/data_sources/${process.env.NOTION_GAMELOG}/query`, {
 			directory: ".cache",
 			duration: '1d',
 			type: 'json',
@@ -10,7 +10,7 @@ export default async function() {
 				method: 'POST',
 				headers: {
 					'Authorization': `Bearer ${process.env.NOTION_API_KEY}`,
-					'Notion-Version': '2022-06-28',
+					'Notion-Version': '2025-09-03',
 					'Content-Type': 'application/json'
 				}
 			}
